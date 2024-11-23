2fc3493c59dd9a933d54913afddf14258768939d
Vulpolox <jack.w.a.keys@gmail.com>
2024-11-23 14:28:34 -0600

    Decided to implement Database Manager as static

---
+++ SOSGame/SOSGame/DatabaseManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOSGame
+{
+    public class DatabaseManager : IDatabaseManager
+    {
+        public DatabaseManager()
+        {
+
+        }
+
+        public bool InitializeDatabase()
+        {
+            return true;
+        }
+
+
+        public List<MoveInfo> GetRecordedMoves()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void RecordMove(MoveInfo moveToAdd)
+        {
+            throw new NotImplementedException ();
+        }
+
+
+        public void ClearRecordedMoves()
+        { 
+            throw new NotImplementedException (); 
+        }
+
+
+        public int GetNumberOfRecordedMoves()
+        {
+            throw new NotImplementedException ();
+        }
+
+
+    }
+}
---
+++ SOSGame/SOSGame/IDatabaseManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOSGame
+{
+    public interface IDatabaseManager
+    {
+        public bool InitializeDatabase();           // self explanatory ; returns true if successful, false otherwise
+        public void RecordMove(MoveInfo moveToAdd); // writes instance data of moveToAdd to a tuple in database table
+        public List<MoveInfo> GetRecordedMoves();   // returns the contents of  table as a list of MoveInfo objects
+        public void ClearRecordedMoves();           // erases all tuples in the database table
+        public int GetNumberOfRecordedMoves();      // returns the number of tuples in the database table
+
+    }
+}
