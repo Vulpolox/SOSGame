45c8904aaee9e40a335a11a043642220b99221e4
Vulpolox <jack.w.a.keys@gmail.com>
2024-11-24 12:54:37 -0600

    Finished DatabaseManager

--- SOSGame/SOSGame/DatabaseManager.cs
+++ SOSGame/SOSGame/DatabaseManager.cs
@@ -1,45 +1,192 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.SQLite;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SOSGame
 {
-    public class DatabaseManager : IDatabaseManager
+    public static class DatabaseManager
     {
-        public DatabaseManager()
-        {
+        // variables
 
+        // hardcoding DB name may be bad practice, but it shouldn't change
+        private static readonly string _connectionString = "Data Source=RecordedMoves.db;Version=3;";
+
+
+        // methods
+
+        public static bool InitializeDatabase()
+        {
+            try
+            {
+                // open a connection to the DB
+                using (SQLiteConnection connection = new SQLiteConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    // query to create a table to store moves
+                    String tableQuery = @"
+                        CREATE TABLE IF NOT EXISTS RecordedMoves (
+                            RowIndex INTEGER,
+                            ColumnIndex INTEGER,
+                            MoveLetter CHAR(1),
+                            PRIMARY KEY (RowIndex, ColumnIndex)
+                        );";
+
+                    // execute the query
+                    using (SQLiteCommand command = new SQLiteCommand(tableQuery, connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                // if the DB initialized correctly, return true
+                return true;
+            }
+
+            catch (Exception ex)
+            {
+                // if there was an error initializing DB
+                Console.WriteLine($"Error initializing DB ; {ex.StackTrace}");
+                return false;
+            }
         }
 
-        public bool InitializeDatabase()
+
+        public static List<MoveInfo> GetRecordedMoves()
         {
-            return true;
+            List<MoveInfo> recordedMoves = new List<MoveInfo>();
+
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection( _connectionString))
+                {
+                    connection.Open();
+
+                    // create SELECT query
+                    String query = @"
+                        SELECT RowIndex, ColumnIndex, MoveLetter
+                        FROM RecordedMoves;";
+
+
+                    // execute and interate through the rows returned by the query
+                    // to create and add MoveInfo objects to the return list
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            MoveInfo moveToAdd = new MoveInfo(
+                                rowIndex: reader.GetInt32(0),
+                                columnIndex: reader.GetInt32(1),
+                                moveLetter: reader.GetString(2)
+                                );
+                           
+                            recordedMoves.Add(moveToAdd);
+                        }
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                // if there was an error querying the recorded moves
+                Console.WriteLine($"Error querying recorded moves ; {ex.StackTrace}");
+            }
+
+            return recordedMoves;
         }
 
 
-        public List<MoveInfo> GetRecordedMoves()
+        public static bool RecordMove(MoveInfo moveToAdd)
         {
-            throw new NotImplementedException();
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    // create INSERT query
+                    String query = @"
+                        INSERT INTO RecordedMoves (RowIndex, ColumnIndex, MoveLetter)
+                        VALUES (@RowIndex, @ColumnIndex, @MoveLetter);";
+
+                    // execute query
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@RowIndex", moveToAdd.RowIndex);
+                        command.Parameters.AddWithValue("@ColumnIndex", moveToAdd.ColumnIndex);
+                        command.Parameters.AddWithValue("@MoveLetter", moveToAdd.MoveLetter);
+
+                        command.ExecuteNonQuery();
+                    }
+                }
+
+                // if insertion successful, return true
+                return true;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error inserting move into table {ex.StackTrace}");
+                return false;
+            }
         }
 
 
-        public void RecordMove(MoveInfo moveToAdd)
+        public static bool ClearRecordedMoves()
         {
-            throw new NotImplementedException ();
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection( _connectionString))
+                {
+                    connection.Open();
+
+                    String query = @"
+                        TRUNCATE TABLE RecordedMoves;";
+
+
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection)) { command.ExecuteNonQuery(); }
+                }
+
+                // return true if successful
+                return true;
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error clearing table {ex.StackTrace}");
+                return false;
+            }
         }
 
 
-        public void ClearRecordedMoves()
-        { 
-            throw new NotImplementedException (); 
-        }
-
-
-        public int GetNumberOfRecordedMoves()
+        public static int GetNumberOfRecordedMoves()
         {
-            throw new NotImplementedException ();
+            try
+            {
+                using (SQLiteConnection connection = new SQLiteConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    String query = @"
+                    SELECT COUNT(*)
+                    FROM RecordedMoves;";
+
+                    // return the number of rows
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection)) { return Convert.ToInt32(command.ExecuteScalar()); }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error retriving number of tuples {ex.StackTrace}");
+                return -1;
+            }
+            
         }
 
 
